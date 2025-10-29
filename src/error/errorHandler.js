d13736c0e7f87f8c1e2d8a8ff7cddf6838d64580
Sanket Ganguly <sanket@itobuz.com>
2025-10-29 14:23:36 +0530

    fix: errorHandler file name casing

---
+++ src/error/errorHandler.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+
+  if (res.statusCode == 200) {
+    res.status(500);
+  }
+
+  const status = res.statusCode;
+  const message =
+    err.message || 'Something went wrong, Please Refresh the Page';
+
+  res.status(status).json({
+    success: false,
+    error: message,
+  });
+};
+
+export default errorHandler;
